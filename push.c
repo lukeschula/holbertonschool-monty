5b040c1932a5061aadaf1b2ef627050ca052ac95
root <schulaluke@gmail.com>
2023-12-18 11:43:32 -0800

    continued if statement inside OG, if special token is NULL print an error message and exit program

--- push.c
+++ push.c
@@ -11,4 +11,12 @@ void push( char *special_token, unsigned int num)
 	
 	if (strcmp(special_token, "push") == 0)
 	{
-		special_token = strtok(NULL, " \t\n");	
+		special_token = strtok(NULL, " \t\n");
+
+		if (special_token == NULL)
+		{
+			dprintf(STDERR_FILENO, "L%u: usage: push integer\n", num);
+			
+			free_all();
+			exit(EXIT_FAILURE);
+		}	
