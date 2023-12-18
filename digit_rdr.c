b2f11c5b4e59e216d36e7aa0f47a2b67873d1de6
root <schulaluke@gmail.com>
2023-12-18 10:46:53 -0800

    updated command function

--- digit_rdr.c
+++ digit_rdr.c
@@ -6,4 +6,22 @@
  */
 int digit_rdr(char *special_token)
 {
-	int x = 
+	int x = 0;
+	
+	while (special_token[x])
+	{
+		if (special_token[0] == '-')
+		{
+			++x;
+			continue;
+		}
+		
+		if (special_token[x] < '0' || special_token[x] > '9')
+			return (0);
+		
+		++x
+
+	}
+	
+	return (1);
+}	
