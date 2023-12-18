ebfec45b27ea06e5734f5c24c54ca153bb2d64ef
root <schulaluke@gmail.com>
2023-12-18 09:42:17 -0800

    started main function and declared variables

---
+++ digit_rdr.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+/**
+ *
+ *
+ *
+ */
+int digit_rdr(char *special_token)
+{
+	int x = 
---
+++ main_function.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/*
+ *
+ *
+ *
+ *
+ */
+int main( int argc, char *argv[])
+{
+	stack_t *stacks;
+	char *input;
+	char *special_token;
+	unsigned int counter;
+	unsigned int x;
+	size_t length;
+
+
