efda996895e3d6cb92d869c032076a169a31c2c9
root <schulaluke@gmail.com>
2023-12-18 11:14:21 -0800

    added function to handle pall, which prints the full stack

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ *
+ *
+ */
+void pall(stack_t **stack, unsigned int num)
+{
+	(void) num;
+	stack_t *fully_stacked;
+
+	if (*stack == NULL)
+		return;
+	fully_stacked = *stack;
+
+	while(fully_stacked != NULL)
+	{
+		printf("%d\n", fully_stacked->n)
+		fully_stacked = fully_stacked->next;
+	}
+}
