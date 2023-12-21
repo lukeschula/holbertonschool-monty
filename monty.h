831117d7e21a089ee98ce24920375e65e9063c0a
root <schulaluke@gmail.com>
2023-12-21 09:11:11 -0800

    added prototype

--- monty.h
+++ monty.h
@@ -43,4 +43,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+int is_dig(char str_num[]);
+void (*sep_func(char *arg))(stack_t **stack, unsigned int line_num);
+void sep_arg(char *file);
+int main(int argc, char **argv);
