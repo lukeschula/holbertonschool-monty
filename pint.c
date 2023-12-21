81b6bd48ae3735dfcc06fa654868e0180ff14b37
root <schulaluke@gmail.com>
2023-12-21 09:40:20 -0800

    complete

--- pint.c
+++ pint.c
@@ -2,5 +2,14 @@
 /**
  *
  */
-void _pall(stack_t **stack, unsigned int line_num)
+void _pint(stack_t **stack, unsigned int line_num)
 {
+	stack_t *top = *stack;
+
+	if (!top->next)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", read->n);
+}
