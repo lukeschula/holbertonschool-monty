5fa3ad9cf5c75517de6edeea8db73ae50ebc0716
root <schulaluke@gmail.com>
2023-12-21 09:31:51 -0800

    completed

--- pall.c
+++ pall.c
@@ -4,4 +4,12 @@
  */
 void _pall(stack_t **stack, unsigned int line_num)
 {
+	stack_t *ptr = *stack;
+	(void) line_num;
 
+	while (ptr->next)
+	{
+		printf("%i\n", ptr->n);
+		ptr = ptr->next;
+	}
+}
