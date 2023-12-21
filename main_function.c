e0bce5f52848e8117728e3a3b565fd634c454e79
root <schulaluke@gmail.com>
2023-12-21 09:01:19 -0800

    fixed helper function

--- main_function.c
+++ main_function.c
@@ -47,4 +47,48 @@ void (*sep_func(char *arg))(stack_t **stack, unsigned int line_num)
 /**
  *
  */
+void sep_arg(char *file)
+{
+	void (*func)(stack_t **stack, unsigned int line_num) = NULL;
+	char *input = NULL;
+	char *argz = NULL;
+	unsigned int line_num = 0;
+	size_t len = 0;
+	FILE fd;
+	stack_t *add_node = NULL;
 
+	add_node = malloc(sizeof(stack_t));
+	if (add_node == NULL)
+		malloc_error();
+	add_node->n = 0;
+	add_node->next = NULL;
+	add_node->prev = NULL;
+	fd = fopen(file, "r");
+	if (!fd)
+		open_error(file);
+	while (getline(&input, &len, fd) != -1)
+	{
+		line_num++;
+		argz = strtok(input, DELIM);
+		if (!argz)
+			continue;
+		func = get_func(argz);
+		if (func == NULL)
+			func_error(line_num, add_node, input, argz, fd);
+		if (func == _push)
+		{
+			nodeValue = strtok(NULL, DELIM);
+			if (!nodeValue || is_dig(nodeValue) == 0)
+				push_error(line_num, add_node, input, fd);
+		}
+		func(&add_node, line_num);
+	}
+	fclose(fd);
+	free(input);
+	free_list(add_node);
+	exit(EXIT_SUCCESS);
+}
+/**
+ *
+ */
+int 
